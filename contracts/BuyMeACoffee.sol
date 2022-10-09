30aa525162819d44893d79672afa4b92c941a2c9
Xander Palmer <xanderpalmer7@gmail.com>
2022-10-09 18:40:30 -0400

    Initialized project, built smart contract

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract BuyMeACoffee {
+    // Event to emit when a Memo is created
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    // Memo struct
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    // List of all memos received from friends
+    Memo[] memos;
+
+    // Address of contract deployer
+    address payable owner;
+
+    // Deploy logic
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev buy a coffee for contract owner
+     * @param _name name of the coffee buyer
+     * @param _message a message from the coffee buyer 
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        require(msg.value > 0, "Can't buy coffee with 0 eth");
+
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name, 
+            _message
+        ));
+        
+        // Emits a log event when memo is created
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+}
